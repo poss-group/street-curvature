f89678f40c7a1aa45973485b1991dfba5409ae60
dbantje <david.bantje@stud.uni-goettingen.de>
2020-09-06 19:51:50 +0200

    AGS.py: Partial AGS codes now allowed

---
+++ networks.py
@@ -0,0 +1,17 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from scipy.spatial import Delaunay
+
+from utils import mercator
+
+class StreetNetwork(object):
+    """
+    """
+    def __init__(self, coordinates):
+        """
+        """
+
+    @classmethod
+    def from_AGS(cls, AGS):
+        """
+        """
