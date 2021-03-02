46f06d01c66b801b46002a644bb22a4d6d062270
dbantje <david.bantje@stud.uni-goettingen.de>
2021-03-02 10:01:05 +0100

    Changed polygon scan to a python function

--- polygon.py
+++ polygon.py
@@ -5,25 +5,10 @@ import osrm
 
 from utils import construct_polygon, measure_polygon
 
-if len(sys.argv) != 9:
-    print("ERROR: Wrong number of arguments.")
-    print("Usage: python polygon.py longitude latitude Nmax Rmin Rmax Rvalues offset_values store_path")
-else:
+def polygon_scan(location, N, R, offset, store_path):
+    """
+    """
     client = osrm.Client(host='http://134.76.24.136/osrm')
-
-    # command line arguments
-    A = np.array([float(sys.argv[1]), float(sys.argv[2])])
-    Nmax = int(sys.argv[3])
-    Rmin = float(sys.argv[4])
-    Rmax = float(sys.argv[5])
-    Rvalues = int(sys.argv[6])
-    offset_values = int(sys.argv[7])
-    store_path = str(sys.argv[8])
-
-    # parameters
-    N = np.arange(3, Nmax+1)
-    R = np.linspace(Rmin, Rmax, Rvalues)
-    offset = np.linspace(0, 2*np.pi, offset_values)
 
     # data containers
     defects = np.zeros((N.size, R.size, offset.size, 2))
@@ -37,8 +22,8 @@ else:
     for k, n in enumerate(N):
         for i, r in enumerate(R):
             for j, alpha in enumerate(offset):
-                B = construct_polygon(n, r, A, offset=alpha)
-                angles, areas, meanR = measure_polygon(A, B, client,
+                B = construct_polygon(n, r, location, offset=alpha)
+                angles, areas, meanR = measure_polygon(location, B, client,
                                                        meanR=True)
                 mean_R[k][i][j] = meanR
                 defects[k][i][j] = 2*np.pi - np.sum(angles, axis=0)
@@ -53,6 +38,7 @@ else:
         os.makedirs(store_path)
 
     # save data
+    np.save(store_path+"/location.npy", location)
     np.save(store_path+"/defects.npy", defects)
     np.save(store_path+"/area_simple.npy", area_simple)
     np.save(store_path+"/area_weighted.npy", area_weighted)
@@ -60,11 +46,3 @@ else:
     np.save(store_path+"/N.npy", N)
     np.save(store_path+"/R.npy", R)
     np.save(store_path+"/offset.npy", offset)
-
-    # write metadata to file
-    logfile = open(store_path+"/polygon.log", 'w')
-    logfile.write("Location: "+str(sys.argv[1])+" "+str(sys.argv[2])+"\n")
-    logfile.write("Edges: "+str(N)+"\n")
-    logfile.write("Polygon circumradii: {} values from {} to {}\n".format(Rvalues, Rmin, Rmax))
-    logfile.write("{} offset values each.\n".format(offset_values))
-    logfile.close()
