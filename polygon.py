38a14239cc8b696a7ff36e8beef299a392c28b0e
dbantje <david.bantje@stud.uni-goettingen.de>
2021-04-15 10:12:43 +0200

    Output of polygon scan now as pandas dataframe

--- polygon.py
+++ polygon.py
@@ -2,6 +2,7 @@ import numpy as np
 import sys, os
 sys.path.append('/home/david/code_repo/osrm-py-master/')
 import osrm
+import pandas as pd
 
 from utils import construct_polygon, measure_polygon
 
@@ -10,11 +11,14 @@ def polygon_scan(location, N, R, offset, store_path):
     """
     client = osrm.Client(host='http://134.76.24.136/osrm')
 
-    # data containers
+    # set up dataframe
     defects = np.zeros((N.size, R.size, offset.size, 2))
     area_simple = np.zeros((N.size, R.size, offset.size, 2))
     area_weighted = np.zeros((N.size, R.size, offset.size, 2))
     mean_R = np.zeros((N.size, R.size, offset.size, 2))
+    R_full = np.zeros((N.size, R.size, offset.size))
+    N_full = np.zeros((N.size, R.size, offset.size), dtype=int)
+    offset_full = np.zeros((N.size, R.size, offset.size))
 
     # calculation
     Npolygons = N.size * R.size * offset.size
@@ -28,8 +32,9 @@ def polygon_scan(location, N, R, offset, store_path):
                 mean_R[k][i][j] = meanR
                 defects[k][i][j] = 2*np.pi - np.sum(angles, axis=0)
                 area_simple[k][i][j] = np.sum(areas, axis=0) / 3
-                area_weighted[k][i][j] = (np.sum(areas*angles, axis=0)
-                                          / np.sum(angles, axis=0))
+                R_full[k][i][j] = r
+                offset_full[k][i][j] = alpha
+                N_full[k][i][j] = n
                 counter += 1
                 print("{} of {} polygons evaluated.".format(counter, Npolygons))
 
@@ -37,12 +42,18 @@ def polygon_scan(location, N, R, offset, store_path):
     if not os.path.exists(store_path):
         os.makedirs(store_path)
 
+    # set up dataframe
+    df = pd.DataFrame()
+    df['defect_duration'] = (defects[:,:,:,0]).flatten()
+    df['area_duration'] = (area_simple[:,:,:,0]).flatten()
+    df['meanR_duration'] = (mean_R[:,:,:,0]).flatten()
+    df['defect_distance'] = (defects[:,:,:,1]).flatten()
+    df['area_distance'] = (area_simple[:,:,:,1]).flatten()
+    df['meanR_distance'] = (mean_R[:,:,:,1]).flatten()
+    df['radius'] = R_full.flatten()
+    df['number of edges'] = N_full.flatten()
+    df['offset'] = offset_full.flatten()
+
     # save data
     np.save(store_path+"/location.npy", location)
-    np.save(store_path+"/defects.npy", defects)
-    np.save(store_path+"/area_simple.npy", area_simple)
-    np.save(store_path+"/area_weighted.npy", area_weighted)
-    np.save(store_path+"/meanR.npy", mean_R)
-    np.save(store_path+"/N.npy", N)
-    np.save(store_path+"/R.npy", R)
-    np.save(store_path+"/offset.npy", offset)
+    df.to_pickle(store_path+"/scan_data.pkl")
